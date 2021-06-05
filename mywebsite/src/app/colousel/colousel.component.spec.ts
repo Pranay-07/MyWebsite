15b8c10fa7051e63244c40faa0b7ca3731be4af1
Kumar,Pranay <Pranay.Kumar@philips.com>
2021-06-05 22:19:37 +0530

    Added Colousel

---
+++ mywebsite/src/app/colousel/colousel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColouselComponent } from './colousel.component';
+
+describe('ColouselComponent', () => {
+  let component: ColouselComponent;
+  let fixture: ComponentFixture<ColouselComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ColouselComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ColouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ mywebsite/src/app/colousel/colousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colousel',
+  templateUrl: './colousel.component.html',
+  styleUrls: ['./colousel.component.scss']
+})
+export class ColouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
