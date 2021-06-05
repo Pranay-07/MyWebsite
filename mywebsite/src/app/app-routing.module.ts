936213ce7a9678afdda52ee984844abf6d191ba6
Kumar,Pranay <Pranay.Kumar@philips.com>
2021-06-06 00:49:55 +0530

    Added routing for Aboutme component

--- mywebsite/src/app/app-routing.module.ts
+++ mywebsite/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
-const routes: Routes = [];
+const routes: Routes = [ { path :'' , component: AboutmeComponent},
+// { path :'ContactMe' , component: ContactMeComponent},
+// { path :'AllProjects' , component: AllProjectsComponent},
+// { path :'AngularProjects' , component: AngularProjectsComponent},
+// { path :'ReactProjects' , component: ReactProjectsComponent},
+// { path :'IOTProjects' , component: IotprojectsComponent},
+// { path :'CppProject' , component: CppprojectsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
